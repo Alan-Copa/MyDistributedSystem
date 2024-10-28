91b00fe1b77814725ea0d70767706456745580dd
Alan Copa <alancopa@Alans-MBP.usilu.net>
2024-10-28 21:09:37 +0100

    task: separare in client handler in e out, manca out

--- Assignments/Ass3/ex2/server_ex2.py
+++ Assignments/Ass3/ex2/server_ex2.py
@@ -64,7 +64,7 @@ def handle_client(conn: socket.socket, addr):
         print(f"[HANDSHAKE] Client {desired_id} connected successfully.")
         
         # Send success response
-        response = FastHandshake(id=assigned_id, error=False)
+        response = FastHandshake(id=desired_id, error=False)
         # response.id = desired_id
         # response.error = False
         # conn.sendall(response.SerializeToString())
